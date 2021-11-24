4e7e777c5900c16318477447ea6eae0da52ff7ac
LOMBATECH <mballohabib93@gmail.com>
2021-11-24 18:05:30 +0000

    mise en place des section skills et services

--- porf.js
+++ porf.js
@@ -14,3 +14,9 @@ $(document).ready(function(){
     });
     
 });
+
+// Skills Bars
+$('.skill-percent').each(function(){
+    $(this).animate({
+      width:$(this).attr('data-percent')},"fast");
+    });
