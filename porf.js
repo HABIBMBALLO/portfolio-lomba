6f0df5b3be2210f0d51504b4cd91a697c27a5dcb
LOMBATECH <mballohabib93@gmail.com>
2021-11-27 02:36:10 +0000

    progress bars

--- porf.js
+++ porf.js
@@ -1,16 +1,25 @@
-$(document).ready(function(){
-    $(window).scrolly(function(){
-        if(this.scrolly > 20){
-            $('..navbar').addClass("stricky");
-        } else{
-            $('..navbar').removeClass("stricky");
+// $(document).ready(function(){
+//     $(window).scrolly(function(){
+//         if(this.scrolly > 20){
+//             $('..navbar').addClass("stricky");
+//         } else{
+//             $('..navbar').removeClass("stricky");
 
-        }
-    });
-    //toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navebar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+//         }
+//     });
+//     //toggle menu/navbar script
+//     $('.menu-btn').click(function(){
+//         $('.navebar .menu').toggleClass("active");
+//         $('.menu-btn i').toggleClass("active");
+//     });
     
-});
+// });
+
+    // Progress bars
+    $(document).ready(function() {
+        $('.progress .progress-bar').css("width",
+                  function() {
+                      return $(this).attr("aria-valuenow") + "%";
+                  }
+          )
+      });
