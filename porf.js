c2c8621bb046f288e4a8e3b61412ec3c880fefdf
LOMBATECH <mballohabib93@gmail.com>
2021-11-27 00:51:44 +0000

    mise en place les realisation

--- porf.js
+++ porf.js
@@ -14,9 +14,3 @@ $(document).ready(function(){
     });
     
 });
-
-// Skills Bars
-$('.skill-percent').each(function(){
-    $(this).animate({
-      width:$(this).attr('data-percent')},"fast");
-    });
