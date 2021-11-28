82d3e9d8c993a127bb70cfa21a3e8f47dc465392
LOMBATECH <mballohabib93@gmail.com>
2021-11-28 11:52:01 +0000

    amelioration

--- porf.js
+++ porf.js
@@ -16,10 +16,10 @@
 // });
 
     // Progress bars
-    $(document).ready(function() {
-        $('.progress .progress-bar').css("width",
-                  function() {
-                      return $(this).attr("aria-valuenow") + "%";
-                  }
-          )
-      });
+    // $(document).ready(function() {
+    //     $('.progress .progress-bar').css("width",
+    //               function() {
+    //                   return $(this).attr("aria-valuenow") + "%";
+    //               }
+    //       )
+    //   });
